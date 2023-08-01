87f0171decadb6066d6b7438d5120a7fb2b4465a
itsspall <muhammadnaufal18@gmail.com>
2023-08-01 09:40:30 +0700

    First Github Project

---
+++ MethodGet/index.php
@@ -0,0 +1,10 @@
+<!-- penanganan form dengan method GET -->
+<form method="get" action="aksi.php">
+    <label>Masukkan Nama</label><br/>
+    <input type="text" name="nama"><br/>
+    
+    <label>Masukkan Usia</label><br/>
+    <input type="text" name="usia"><br/>
+    
+    <input type="submit" value="oke">
+</form>
---
+++ MethodPost/index.php
@@ -0,0 +1,10 @@
+<!-- penanganan form dengan method POST -->
+<form method="post" action="aksi.php">
+    <label>Masukkan Nama</label><br/>
+    <input type="text" name="nama"><br/>
+
+    <label>Masukkan Usia</label><br/>
+    <input type="text" name="usia"><br/>
+    <br>
+    <input type="submit" value="oke">
+</form>
